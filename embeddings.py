2f0862fff9e489b5b81d3449bf37f4611f57e5e3
kenshin-alpha <uthejkarnam@gmail.com>
2026-03-23 17:49:28 +0000

    Creating a function to convert strings into TF-IDF vectors

--- embeddings.py
+++ embeddings.py
@@ -1,1 +1,11 @@
 #Methods related to converting text in into numeric representation and then returning numeric representation may go here
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from Config import Config
+
+def get_tfidf_embd(df: pd.DataFrame):
+    combined_text = df[Config.TICKET_SUMMARY] + " " + df[Config.INTERACTION_CONTENT]
+    vectorizer = TfidfVectorizer(max_features=1000, stop_words='english')
+    X = vectorizer.fit_transform(combined_text)
+    return X
