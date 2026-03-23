32d6a6e093ac60110ded89d0f7987eeafd2b74b2
chandrapooja1225-droid <chandrapooja1225@gmail.com>
2026-03-23 15:56:36 +0000

    Loading the datasets

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,15 @@
 #Methods related to data loading and all pre-processing steps will go here
+
+import os
+import pandas as pd
+from Config import Config
+
+def get_input_data():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    df1 = pd.read_csv(os.path.join(base_dir, "data/AppGallery.csv"))
+    df2 = pd.read_csv(os.path.join(base_dir, "data/Purchasing.csv"))
+    df = pd.concat([df1, df2], ignore_index=True)
+    df = df.dropna(subset=Config.TYPE_COLS)
+    df[Config.INTERACTION_CONTENT] = df[Config.INTERACTION_CONTENT].fillna("")
+    df[Config.TICKET_SUMMARY] = df[Config.TICKET_SUMMARY].fillna("")
+    return df
