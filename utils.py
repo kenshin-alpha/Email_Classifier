a3f96f46539fac304855abc781fc46d88852bafe
chandrapooja1225-droid <chandrapooja1225@gmail.com>
2026-03-23 17:30:48 +0000

    Creating functions for safe encoding and concatenation of data

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
 #Any extra functionality that need to be reused will go here
+
+import numpy as np
+from scipy.sparse import hstack
+
+def concat_features(X, y_encoded):
+    y_2d = np.array(y_encoded).reshape(-1, 1)
+    if hasattr(X, "tocsr"):
+        return hstack([X, y_2d]).tocsr()
+    else:
+        return np.hstack([X, y_2d])
+
+def encode_safe(le, labels):
+    classes = list(le.classes_)
+    return np.array([classes.index(l) if l in classes else 0 for l in labels])
