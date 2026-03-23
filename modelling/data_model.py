2c189568caea19fafd8edc7a2628c0bbae922d96
akanksha2430 <girmajiakanksha@gmail.com>
2026-03-23 15:47:29 +0000

    Data Object changes

--- modelling/data_model.py
+++ modelling/data_model.py
@@ -11,10 +11,30 @@ np.random.seed(seed)
 class Data():
     def __init__(self,
                  X: np.ndarray,
-                 df: pd.DataFrame) -> None:
-                 # This method will create the model for data
-                 #This will be performed in second activity
-
+                 df: pd.DataFrame = None,
+                 X_train=None, X_test=None, y_train=None, y_test=None,
+                 train_df=None, test_df=None) -> None:
+        if X_train is not None and y_train is not None:
+            self.X_train = X_train
+            self.X_test = X_test
+            self.y_train = y_train
+            self.y_test = y_test
+            self.train_df = train_df
+            self.test_df = test_df
+            self.embeddings = X
+            self.df = df
+            self.y = y_train if y_train is not None else None
+        else:
+            self.embeddings = X
+            self.df = df
+            self.y = df[Config.TYPE_COLS]
+            
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+                X, self.y, test_size=0.2, random_state=seed
+            )
+            self.train_df, self.test_df = train_test_split(
+                df, test_size=0.2, random_state=seed
+            )
 
     def get_type(self):
         return  self.y
