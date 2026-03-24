f9c19084da1c560b1a446151640657845fdd5deb
kenshin-alpha <uthejkarnam@gmail.com>
2026-03-24 11:54:20 +0000

    Chained Design Implementation of model for Type 4 Classifier

--- modelling/modelling.py
+++ modelling/modelling.py
@@ -12,6 +12,7 @@ def model_predict(data, df, name):
     if name == 'chained':
         y2_train = data.y_train[Config.TYPE_COLS[0]]
         y3_train = data.y_train[Config.TYPE_COLS[1]]
+        y4_train = data.y_train[Config.TYPE_COLS[2]]
         
         # Modelling for y2
         model_y2 = RandomForest("y2", data.embeddings, y2_train)
@@ -27,7 +28,7 @@ def model_predict(data, df, name):
         y2_enc_train = le_y2.fit_transform(y2_train)
         preds_y2_enc = encode_safe(le_y2, preds_y2)
         
-        # 2. Model for y3
+        # Modelling for y3
         X_train_y3 = concat_features(data.X_train, y2_enc_train)
         X_test_y3 = concat_features(data.X_test, preds_y2_enc)
         
@@ -47,12 +48,22 @@ def model_predict(data, df, name):
         X_train_y4 = concat_features(X_train_y3, y3_enc_train)
         X_test_y4 = concat_features(X_test_y3, preds_y3_enc)
 
+        # Modelling for y4
+        model_y4 = RandomForest("y4", data.embeddings, y4_train)
+        d_y4 = Data(X=data.embeddings, df=data.df, X_train=X_train_y4, X_test=X_test_y4,
+                    y_train=y4_train, y_test=data.y_test[Config.TYPE_COLS[2]],
+                    train_df=data.train_df, test_df=data.test_df)
+        model_y4.train(d_y4)
+        model_y4.predict(X_test_y4)
+        preds_y4 = model_y4.prediction
+
         return {
             "name": "chained",
-            "models": [model_y2, model_y3],
+            "models": [model_y2, model_y3, model_y4],
             "predictions": pd.DataFrame({
                 Config.TYPE_COLS[0]: preds_y2,
-                Config.TYPE_COLS[1]: preds_y3
+                Config.TYPE_COLS[1]: preds_y3,
+                Config.TYPE_COLS[2]: preds_y4
             })
         }
     return None
